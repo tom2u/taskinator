5e1ec7cf1796be09a0575bd298e10b1143433d2b
tom2u <tbrodhead@2u.com>
2020-03-17 13:23:08 -0500

    4.3.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
@@ -10,7 +11,7 @@ var taskFormHandler = function(event) {
         alert("You need to fill out the task form!");
         return false;
     }
-    //formEl.reset();
+    formEl.reset();
     // package up data as an object
     var taskFormData = {
         name: taskNameInput,
@@ -22,21 +23,21 @@ var taskFormHandler = function(event) {
 };
 
 var createTaskEl = function(taskDataObj) {
-    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-
-    // create div to hold task info and add to list item
+  
+    // add task id as a custom attribute
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
+  
     var taskInfoEl = document.createElement("div");
-    // give it a class name
     taskInfoEl.className = "task-info";
-    // add HTML content to div
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-
     listItemEl.appendChild(taskInfoEl);
-
-    // add entire list item to list
+  
     tasksToDoEl.appendChild(listItemEl);
-}
+  
+    // increase task counter for next unique id
+    taskIdCounter++;
+  };
 
 formEl.addEventListener("submit", taskFormHandler);
